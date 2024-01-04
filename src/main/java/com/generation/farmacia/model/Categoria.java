068647bf6ed0da65823511faf68b7b712d8f9eb3
Renan Nunes <rafaelnc22@gmail.com>
2024-01-04 12:39:35 -0300

    Crud Produto

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,10 +1,17 @@
 package com.generation.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -26,9 +33,10 @@ public class Categoria {
 	@Size(min = 10, max = 500, message = "A descrição da categoria deve ter no minimo 10 e no maximo 500 caracteres.")
 	@Column(length = 500)
 	private String descricao;
-	
-	@Column(columnDefinition="tinyint(1) default 1")
-	private boolean disponivel;
+		
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("categoria")
+    private List<Produto> produto;
 
 	public long getId() {
 		return id;
@@ -53,14 +61,16 @@ public class Categoria {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-
-	public boolean isDisponivel() {
-		return disponivel;
+	
+	
+	public List<Produto> getProduto() {
+		return produto;
 	}
 
-	public void setDisponivel(boolean disponivel) {
-		this.disponivel = disponivel;
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 
 	
 }
+	
---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,117 @@
+package com.generation.farmacia.model;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "Nome obrigatorio, não pode ser em branco!")
+	@Size(min = 5, max = 30)
+	@Column(length = 30)
+	private String nome;
+
+	@NotBlank(message = "Descrição não pode estar em branco")
+	@Size(min = 5, max = 100)
+	@Column(length = 100)
+	private String descricao;
+
+	private LocalDateTime dataValidade;
+
+	@NotNull(message = "Quantidade não pode estar em branco")
+	private int quantidade;
+
+	@NotBlank(message = "Foto não pode estar em branco")
+	private String foto;
+
+	@NotNull(message = "Informe o Preço do Produto!")
+	@Positive(message = "O preço do Produto deve ser maior que zero!")
+	private float preco;	
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDateTime getDataValidade() {
+		return dataValidade;
+	}
+
+	public void setDataValidade(LocalDateTime dataValidade) {
+		this.dataValidade = dataValidade;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+			
+	
+}
